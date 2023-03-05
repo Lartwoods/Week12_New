32224da477d62275f7f4335ddcf667dcdef3fd4c
Vlada <lartwoods@gmail.com>
2023-03-05 16:52:36 +0300

    added script

--- delete/delete_script.js
+++ delete/delete_script.js
@@ -1,4 +1,5 @@
-const str = document.querySelector(".all");
+const str =
+  "<h1>Текст, в <br />котором нужно<col />убрать<embed />всe<hr />лишние<img />теги</h1>";
 function deleteTags(str) {
   let regex = /( |<([^>]+)>)/gi,
     result = str.replace(regex, " ");
